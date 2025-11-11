2ebbd09f3c234391e4939003da0ec63414683610
Mobile User <mobile@localhost>
2025-11-11 14:03:38 +0300

    save local changes at 2025-11-11 14:03:08

--- myGame/GameScene.lua
+++ myGame/GameScene.lua
@@ -1,3 +1,10 @@
 local Scene = require("engine.classes.Scene")
 
 local GameScene = {}
+GameScene.__index = GameScene
+
+function GameScene.new()
+    local instance = Scene.new("GameScene")
+    setmetatable(instance,Scene)
+    return instance
+end
