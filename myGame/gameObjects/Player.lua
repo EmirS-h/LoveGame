1a48f77e2e7dc6c35430970e3c18d0947aef1a20
Mobile User <mobile@localhost>
2025-11-11 15:36:10 +0300

    save local changes at 2025-11-11 15:36:10

--- myGame/gameObjects/Player.lua
+++ myGame/gameObjects/Player.lua
@@ -1,14 +1,24 @@
 local GameObject = require("engine.classes.GameObject")
 local Transform = require("engine.components.Transform")
 local Shape = require("engine.components.Shape")
+-- Require our new behavior
+local ScreenBounce = require("engine.components.ScreenBounce")
 
 local Player = {}
 
 function Player:new()
     local go = GameObject:new()
 
-    go:add(Transform:new(150,300))
-    go:add(Shape:new(nil,100,200))
+    -- 1. Add Transform (Position)
+    go:add(Transform:new(150, 300))
+
+    -- 2. Add Shape (Visuals) - Red Rectangle
+    -- (type, width, height, color)
+    go:add(Shape:new("rectangle", 50, 50, {0, 0, 1}))
+    
+    -- 3. Add Behavior (Logic)
+    -- Give it X velocity of 200 and Y velocity of 150
+    go:add(ScreenBounce:new(200, 150))
     
     return go
 end
